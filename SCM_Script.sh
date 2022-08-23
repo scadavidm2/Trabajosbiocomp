e2efbd3b54c4aedddf25181e30d11cb5e901853a
scadavidm2 <scadavidm2@eafit.edu.co>
2022-08-23 17:11:35 -0500

    Ultimos cambios a los archivos

--- SCM_Script.sh
+++ SCM_Script.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+tail -n +2 sars_cov2.fasta > sars_cov_2.txt
+
+tr -d "\n" <  sars_cov_2.txt > sars_cov_2.tmp && mv sars_cov_2.tmp sars_cov_2.txt
+
+Semidoctorado=$(cat sars_cov_2.txt)
+
+echo "${Semidoctorado//GATATC/GAT ATC}"
+
+tr ' ' '\n' < sars_cov_2.txt > sars_cov_2.tmp && mv sars_cov_2.tmp sars_cov_2.txt
+
+awk '{ print length }' sars_cov_2.txt > sars_cov_2.tmp && mv sars_cov_2.tmp sars_cov_2.txt
+
+sort -nr sars_cov_2.txt > sars_cov_2.tmp && mv sars_cov_2.tmp sars_cov_2.txt
 
